a5526c8e231c0453e2c9f0dee42590aadc5a0d6a
OlegVesel <olegich1406@gmail.com>
2021-11-06 23:21:19 +0300

    added LAZY annotation

--- src/main/java/com/home/ApplicationCosmetos/Config/WebSecurityConfig.java
+++ src/main/java/com/home/ApplicationCosmetos/Config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.home.ApplicationCosmetos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private PasswordEncoder passwordEncoder;
 
     @Bean
+    @Lazy
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder(8);
     }
