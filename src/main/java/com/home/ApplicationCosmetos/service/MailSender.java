2b3ab417bc299942ef34fa2d5f92f3bbc6906870
OlegVesel <olegich1406@gmail.com>
2021-11-06 23:46:53 +0300

    rename MailConfig -> MailService

--- src/main/java/com/home/ApplicationCosmetos/service/MailSender.java
+++ src/main/java/com/home/ApplicationCosmetos/service/MailSender.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class MailSender {
 
     @Value("${spring.mail.username}")
     private String username;
-
+    @Async
     public void sendMail(String emailTo, String titleEmail, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
