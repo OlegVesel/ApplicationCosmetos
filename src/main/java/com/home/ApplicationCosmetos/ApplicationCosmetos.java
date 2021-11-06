317c23c7821749ce125777f51d70f07b9352d190
OlegVesel <olegich1406@gmail.com>
2021-11-06 23:06:30 +0300

    delete async anotation

--- src/main/java/com/home/ApplicationCosmetos/ApplicationCosmetos.java
+++ src/main/java/com/home/ApplicationCosmetos/ApplicationCosmetos.java
@@ -2,9 +2,7 @@ package com.home.ApplicationCosmetos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class ApplicationCosmetos {
 
--- src/main/java/com/home/ApplicationCosmetos/service/MailSender.java
+++ src/main/java/com/home/ApplicationCosmetos/service/MailSender.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +14,6 @@ public class MailSender {
     @Value("${spring.mail.username}")
     private String username;
 
-    @Async
     public void sendMail(String emailTo, String titleEmail, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
